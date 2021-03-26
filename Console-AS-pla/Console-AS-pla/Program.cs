d9648c84395910c4ce0b2c8ea17aa0118779533a
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-27 00:54:08 +0900

    配列からの取り出し　練習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Console_AS_pla
 {
@@ -6,7 +7,59 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            One(numbers);
+            Console.WriteLine("-----");
+
+            Two(numbers);
+            Console.WriteLine("-----");
+
+            Three(numbers);
+            Console.WriteLine("-----");
+
+            Four(numbers);
+            Console.WriteLine("-----");
+
+            Five(numbers);
+            Console.WriteLine("-----");
+        }
+
+        private static void One(int[] numbers)
+        {
+            var max = numbers.Max();
+            Console.WriteLine(max);
+        }
+
+        private static void Two(int[] numbers)
+        {
+            var skip = numbers.Length - 2;
+            foreach (var n in numbers.Skip(skip))
+                Console.WriteLine(n);
+        }
+
+        private static void Three(int[] numbers)
+        {
+            var str = numbers.Select(n => n.ToString());
+            foreach (var s in str)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Four(int[] numbers)
+        {
+            foreach (var num in numbers.OrderBy(n => n).Take(3))
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        private static void Five(int[] numbers)
+        {
+            var num = numbers.Distinct().Count(n => n > 10);
+            Console.WriteLine(num);
+
         }
     }
 }
