96a71cd43336130e6dc6762cdea9e34446cd64ec
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-26 23:05:01 +0900

    project create

---
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_AS_pla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
