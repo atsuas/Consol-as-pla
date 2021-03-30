54e379a970c9b80dd3d36a26f08c72416c9fb485
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-31 06:49:55 +0900

    不要なコードの削除

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -8,24 +8,7 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
-            var abbrs = new Abbreviations();
-
-            abbrs.Add("IOC", "国際オリンピック委員会");
-            abbrs.Add("NPI", "核兵器不拡散条約");
-
-            var count = abbrs.Count;
-            Console.WriteLine(count);
-            Console.WriteLine();
-
-            if (abbrs.Remove("NPI"))
-                Console.WriteLine(count);
-            if (!abbrs.Remove("NPI"))
-                Console.WriteLine("削除できません");
-            Console.WriteLine();
-
-            foreach (var item in abbrs.Where(x => x.Key.Length == 3))
-                Console.WriteLine($"{item.Key} = {item.Value}");
-
+            
         }
 
     }
