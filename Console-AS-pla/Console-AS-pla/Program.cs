34175f6da64c997d62e4028a75490603fcf7f0bc
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-31 05:31:47 +0900

    ディクショナリを使ったサンプルプログラム　練習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -8,8 +8,35 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
-            
+            var abbrs = new Abbreviations();
 
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPI", "核兵器不拡散条約");
+
+            var names = new[] { "WHO", "FIFA", "NPI", };
+            foreach (var name in names)
+            {
+                var fullname = abbrs[name];
+                if (fullname == null)
+                    Console.WriteLine($"{name}は見つかりません");
+                else
+                    Console.WriteLine($"{name}={fullname}");
+            }
+            Console.WriteLine();
+
+            var japanese = "東南アジア諸国連合";
+            var abbreviation = abbrs.ToAbbreviation(japanese);
+            if (abbreviation == null)
+                Console.WriteLine($"{japanese}は見つかりません");
+            else
+                Console.WriteLine($"「{japanese}」の略語は{abbreviation}です");
+            Console.WriteLine();
+
+            foreach (var item in abbrs.FindAll("国際"))
+            {
+                Console.WriteLine($"{item.Key}={item.Value}");
+            }
+            Console.WriteLine();
         }
 
     }
