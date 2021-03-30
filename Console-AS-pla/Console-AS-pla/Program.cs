43d65f7626b89b7255687bf77247c0dcf663e10e
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-30 22:19:32 +0900

    不要なコードの削除

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -6,9 +6,45 @@ namespace Console_AS_pla
 {
     class Program
     {
-        static void Main(string[] args)
+        public class YearMonth
         {
-            
+            public int Year { get; private set; }
+
+            public int Month { get; private set; }
+
+            public YearMonth(int year, int month)
+            {
+                Year = year;
+                Month = month;
+            }
+
+            // 4.1.2
+            public bool Is21Century
+            {
+                get
+                {
+                    return 2001 <= Year && Year <= 2100;
+                }
+            }
+
+            // 4.1.3
+            public YearMonth AddOneMonth()
+            {
+                if (Month == 12)
+                {
+                    return new YearMonth(this.Year + 1, 1);
+                }
+                else
+                {
+                    return new YearMonth(this.Year, this.Month + 1);
+                }
+            }
+
+            // 4.1.4
+            public override string ToString()
+            {
+                return $"{Year}年{Month}月";
+            }
         }
     }
 }
