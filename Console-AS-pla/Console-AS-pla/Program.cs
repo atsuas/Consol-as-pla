758ab5f585ee3cb4b1252502fb4e4f84ffe199f5
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-31 07:25:31 +0900

    ファイルの作成　練習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace Console_AS_pla
 {
@@ -8,7 +9,14 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
-            
+            var lines = new string[]
+            {
+                "ああああああああああ、あああああああ",
+                "いいいいいいいいいいいいいいいいいいいいい",
+                "ううううううううううううううううううううううううう",
+                "ええええええええええええええええええええええええええええ"
+            };
+            File.WriteAllLines(@"C:\temp\祇園精舎.txt", lines);
         }
 
     }
