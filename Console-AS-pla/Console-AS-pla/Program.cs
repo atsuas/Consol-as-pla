c577c94a61ba9f3df0f7ddf9d70ff66c476726c1
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-31 02:58:08 +0900

    Dictionaryを使う　練習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -8,7 +8,47 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
-            
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            One(text);
+            Console.WriteLine();
+            Two(text);
+
+        }
+
+        static void One(string text)
+        {
+            var dict = new Dictionary<Char, int>();
+            foreach (var c in text)
+            {
+                var uc = char.ToUpper(c);
+                if ('A' <= uc && uc <= 'Z')
+                {
+                    if (dict.ContainsKey(uc))
+                        dict[uc]++;
+                    else
+                        dict[uc] = 1;
+                }
+            }
+            foreach (var item in dict.OrderBy(x => x.Key))
+                Console.WriteLine($"{item.Key} : {item.Value}");
+        }
+
+        static void Two(string text)
+        {
+            var dict = new SortedDictionary<Char, int>();
+            foreach (var c in text)
+            {
+                var uc = char.ToUpper(c);
+                if ('A' <= uc && uc <= 'Z')
+                {
+                    if (dict.ContainsKey(uc))
+                        dict[uc]++;
+                    else
+                        dict[uc] = 1;
+                }
+            }
+            foreach (var item in dict)
+                Console.WriteLine($"{item.Key} : {item.Value}");
         }
     }
 }
