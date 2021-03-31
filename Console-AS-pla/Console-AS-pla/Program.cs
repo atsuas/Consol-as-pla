a35283ccc3239a5c229d082fac5d7bfbfa024d40
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-01 06:26:27 +0900

    Yを入力したら大文字、Nを入力したらそのまま入力される

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-namespace Console_AS_pla
+namespace AS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var s = Console.ReadLine();
+            var t = Console.ReadLine();
+            if (s == "Y")
+                Console.WriteLine(t.ToUpper());
+            else
+                Console.WriteLine(t);
         }
 
     }
