172a12556257dc76e279ce62f514d734b6198c77
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-31 21:48:46 +0900

    StreamReaderを使って一行ずつ読み込む

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,8 +9,45 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
-           
+            if (args.Length == 0)
+                return;
+            var file = args[0];
+
+            Console.WriteLine($"{Path.GetFullPath(file)}について調べます");
+            One(file);
+            Two(file);
+            Three(file);
+
         }
 
+        private static void One(string file)
+        {
+            var count = 0;
+            using (var sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (line.Contains(" class "))
+                        count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        private static void Two(string file)
+        {
+            var count = File.ReadAllLines(file)
+                            .Count(s => s.Contains(" class "));
+            Console.WriteLine(count);
+        }
+
+        private static void Three(string file)
+        {
+            var count = File.ReadLines(file)
+                            .Count(s => s.Contains(" class "));
+            Console.WriteLine(count);
+
+        }
     }
 }
