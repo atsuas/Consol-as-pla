17375696d4112b46eeefa2b083108cb4106053aa
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-31 20:00:59 +0900

    ファイルの作成　練習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,12 +9,21 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines(@"C:\temp\祇園精舎.txt");
-            foreach (var item in lines)
-            {
-                Console.WriteLine(item);
-            }
+            var path = @"C:\temp\祇園精舎.txt";
+            CreatePath(path);
 
+
+            static void CreatePath(string path)
+            {
+                var lines = new string[]
+                {
+                    "祇園精舍の鐘の声、諸行無常の響きあり。",
+                    "娑羅双樹の花の色、盛者必衰の理をあらはす。",
+                    "奢れる人も久しからず、ただ春の夜の夢のごとし。",
+                    "猛き者もつひにはほろびぬ、ひとへに風の前の塵に同じ。"
+                };
+                File.WriteAllLines(path, lines);
+            }
         }
 
     }
