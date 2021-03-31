d9203a36fcd9ecf0d6d5158f39bdf890f61eec5f
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-31 22:21:18 +0900

    テキストを読み込み、行番号を振って、別のファイルに出力する

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,9 +9,28 @@ namespace Console_AS_pla
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+                return;
+            var file = args[0];
+            var outputPath = Numberring(file);
 
+            Display(outputPath);
 
         }
 
+        private static string Numberring(string file)
+        {
+            var lines = File.ReadLines(file)
+                            .Select((s, n) => string.Format("{0,4}{1}", n + 1, s));
+            var path = Path.ChangeExtension(file, ".txt");
+            File.WriteAllLines(path, lines);
+            return path;
+        }
+
+        private static void Display(string outputPath)
+        {
+            var text = File.ReadAllText(outputPath);
+            Console.WriteLine(text);
+        }
     }
 }
