52ca39ed7c8dce5c572deb087d051f45289553c8
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-03 05:47:03 +0900

    Listの練習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,42 +9,60 @@ namespace AS
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
-
-            One(numbers);
-            Console.WriteLine();
-
-            Two(numbers);
-            Console.WriteLine();
-
-            Three(numbers);
-            Console.WriteLine();
-        }
-
-        private static void One(List<int> numbers) 
-        {
-            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
-            if (exist)
-                Console.WriteLine("存在します");
-            else
-                Console.WriteLine("存在していません");
-        }
-
-        private static void Two(List<int> numbers)
-        {
-            var fe = numbers.Select(n => n / 2.0);
-            foreach (var number in fe)
+            var names = new List<string>
             {
-                Console.WriteLine(number);
+                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            //One(names);
+            //Console.WriteLine();
+
+            Two(names);
+            Console.WriteLine();
+
+            Three(names);
+            Console.WriteLine();
+
+            Four(names);
+            Console.WriteLine();
+        }
+
+        private static void One(List<string> names)
+        {
+            do
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+                var index = names.FindIndex(s => s == line);
+                Console.WriteLine(index);
+            } while (true);
+            
+        }
+
+        private static void Two(List<string> names)
+        {
+            var count = names.Count(s => s.Contains("o"));
+            Console.WriteLine(count);               
+        }
+
+        private static void Three(List<string> names)
+        {
+            var array = names.Where(s => s.Contains("o"))
+                             .ToArray();
+            foreach (var ar in array)
+            {
+                Console.WriteLine(ar);
             }
         }
 
-        private static void Three(List<int> numbers)
+        private static void Four(List<string> names)
         {
-            var fifth = numbers.Where(n => n >= 50);
-            foreach (var number in fifth)
+            var park = names.Where(n => n.StartsWith("B"))
+                            .Select(s => s.Length);
+            foreach (var item in park)
             {
-                Console.WriteLine(number);
+                Console.WriteLine(item);
             }
         }
     }
