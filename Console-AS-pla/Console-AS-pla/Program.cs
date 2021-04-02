da37bde3e07db8428fbdda8c36d7f792434df380
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-03 01:49:13 +0900

    プロパティの設定　コンストラクタを設定

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -13,4 +13,18 @@ namespace AS
         }
 
     }
+
+    class Song
+    {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        public Song(string title, string artistname, int length)
+        {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
 }
