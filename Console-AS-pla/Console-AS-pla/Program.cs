d6416597dbc1c26ce319263d07c29d49cb02ec38
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-03 02:41:14 +0900

    song class

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,7 +9,23 @@ namespace AS
     {
         static void Main(string[] args)
         {
-           
+            var songs = new Song[]
+            {
+               new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+
+        }
+
+        private static void PrintSongs(Song[] songs )
+        {
+            foreach (var song in songs)
+            {
+                Console.WriteLine(@"{0} {1} {2}", )
+            }
         }
 
     }
