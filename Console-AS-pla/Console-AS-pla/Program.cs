31f8a41807aa559b72cb13e2ca134623dc98ba73
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-03 21:29:15 +0900

    同じ文字列か判定する　練習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,8 +9,13 @@ namespace AS
     {
         static void Main(string[] args)
         {
-
-
+            var one = Console.ReadLine();
+            var two = Console.ReadLine();
+            if (string.Compare(one, two, ignoreCase: true) == 0)
+                Console.WriteLine("同じです");
+            else
+                Console.WriteLine("等しくありません");
+                
         }
     }
 
