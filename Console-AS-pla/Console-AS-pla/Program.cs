ef9b54409388544a88deda2950a8e6335caa502f
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-04 21:49:21 +0900

    不要なコードの削除

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,53 +9,9 @@ namespace AS
     {
         static void Main(string[] args)
         {
-            var text = "Jackdaws love my big sphinx of quartz";
-
-            One(text);
-            Console.WriteLine();
-
-            Two(text);
-            Console.WriteLine();
-
-            Three(text);
-            Console.WriteLine();
-
-            Four(text);
-            Console.WriteLine();
+            
         }
 
-        private static void One(string text)
-        {
-            int space = text.Count(s => s == ' ');
-            Console.WriteLine(space);
-        }
-
-        private static void Two(string text)
-        {
-            var replaced = text.Replace("big", "small");
-            Console.WriteLine(replaced);
-        }
-
-        private static void Three(string text)
-        {
-            var count = text.Split(' ').Length;
-            Console.WriteLine(count);
-        }
-
-        private static void Four(string text)
-        {
-            var word = text.Split(' ')
-                           .Where(s => s.Length <= 4);
-            foreach (var item in word)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
-        private static void Five(string text)
-        {
-
-        }
     }
 
 }
