df8476835e5ba87010b5f9828e99bae474d42f4e
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-05 00:34:55 +0900

    配列からの取得

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,9 +9,61 @@ namespace AS
     {
         static void Main(string[] args)
         {
-            
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            One(numbers);
+            Console.WriteLine();
+
+            Two(numbers);
+            Console.WriteLine();
+
+            Three(numbers);
+            Console.WriteLine();
+
+            Four(numbers);
+            Console.WriteLine();
+
+            Five(numbers);
+
         }
 
+        private static void One(int[] numbers)
+        {
+            var small = numbers.Max();
+            Console.WriteLine(small);
+        }
+
+        private static void Two(int[] numbers)
+        {
+            var skip = numbers.Length - 2;
+            foreach (var item in numbers.Skip(skip))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Three(int[] numbers)
+        {
+            var st = numbers.Select(x => x.ToString());
+            foreach (var n in st)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Four(int[] numbers)
+        {
+            foreach (var n in numbers.OrderBy(c => c).Take(3))
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Five(int[] numbers)
+        {
+            var ten = numbers.Distinct().Count(x => x >= 10);
+            Console.WriteLine(ten);
+        }
     }
 
 }
