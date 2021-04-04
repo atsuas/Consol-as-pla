835d2d5de3c9a058917b8442709a0496b2dce16a
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-04 21:48:30 +0900

    文字列から取得する

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,12 +9,52 @@ namespace AS
     {
         static void Main(string[] args)
         {
-            var line = Console.ReadLine();
-            int num;
-            if (int.TryParse(line, out num))
-                Console.WriteLine("{0:#,#}", num);
-            else
-                Console.WriteLine("数値文字列ではありません");
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            One(text);
+            Console.WriteLine();
+
+            Two(text);
+            Console.WriteLine();
+
+            Three(text);
+            Console.WriteLine();
+
+            Four(text);
+            Console.WriteLine();
+        }
+
+        private static void One(string text)
+        {
+            int space = text.Count(s => s == ' ');
+            Console.WriteLine(space);
+        }
+
+        private static void Two(string text)
+        {
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
+        }
+
+        private static void Three(string text)
+        {
+            var count = text.Split(' ').Length;
+            Console.WriteLine(count);
+        }
+
+        private static void Four(string text)
+        {
+            var word = text.Split(' ')
+                           .Where(s => s.Length <= 4);
+            foreach (var item in word)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Five(string text)
+        {
+
         }
     }
 
