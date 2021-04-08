11e99ba379ed044da567a4c9694b56a608822aa5
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-09 01:15:59 +0900

    price復習

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,45 +9,15 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-           
-
-        }
-
-        
-    }
-
-    class Abbreviations
-    {
-        private Dictionary<string, string> _dict = new Dictionary<string, string>();
-
-        public Abbreviations()
-        {
-            var lines = File.ReadLines("Abbreviations.txt");
-            _dict = lines.Select(line => line.Split('='))
-                         .ToDictionary(x => x[0], x => x[1]);
-        }
-
-        public void Add(string abbr, string japanese)
-        {
-            _dict[abbr] = japanese;
-        }
-
-        public string this [string abbr]
-        {
-            get
+            var line = Console.ReadLine();
+            var price = int.Parse(line);
+            if (price >= 0)
             {
-                return _dict.ContainsKey(abbr) ? _dict[abbr] : null;
+                var totalprice = price * 1.1;
+                Console.WriteLine($"{totalprice:0}");
             }
-        }
-
-        public string ToAbbreviation(string japanese)
-        {
-            return _dict.FirstOrDefault(x => x.Value == japanese).Key;
-        }
-
-        public IEnumerable<KeyValuePair<string, string>> FindAll(string substring)
-        {
 
         }
+
     }
 }
