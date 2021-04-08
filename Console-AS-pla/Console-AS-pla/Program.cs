75570c0d25551cd41bd2aa45b3fc352e43089fe8
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-08 23:08:07 +0900

    ディクショナリから要素をすべて取り出す

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,26 +9,19 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            foreach (var pair in line.Split(';'))
+            //ディクショナリから全ての要素を取り出す
+            foreach (var item in flowerDict)
+                Console.WriteLine($"{item.Key} {item.Value}");
+
+            var average = flowerDict.Average(x => x.value);
+
+            foreach (var key in flowerDict.Keys)
             {
-                var array = pair.Split('=');
-                Console.WriteLine("{0}:{1}", ToJapanese(array[0]), array[1]);
+                Console.WriteLine(key);
             }
+
+
         }
 
-        static string ToJapanese(string key)
-        {
-            switch (key)
-            {
-                case "Novelist":
-                    return "作家　";
-                case "BestWork":
-                    return "代表作";
-                case "Born":
-                    return "誕生年";
-            }
-            throw new ArgumentException("引数keyは、正しい値ではありません");
-        }
     }
 }
