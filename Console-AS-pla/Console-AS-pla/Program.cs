bed1d5314744389c032d9b905746bd7973b8a966
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-10 00:11:43 +0900

    テキストファイルを一行ずつ読み込む

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,21 +9,19 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            var array2d = new string[,]
+            var filePath = @"C:\Example\Greeting.txt";
+            if (File.Exists(filePath))
             {
-               { "sun" , "日曜日" },
-                { "mon" , "月曜日" },
-                { "tue" , "火曜日" },
-                { "wed" , "水曜日" },
-                { "thu" , "木曜日" },
-                { "fri" , "金曜日" },
-                { "sat" , "土曜日" }
-            };
-
-            for (int row = 0; row < array2d.GetLength(0); row++)
-            {
-                Console.WriteLine($"{array2d[row, 1]} {array2d[row, 0]}");
+                using (var reader = new StreamReader(filePath, Encoding.UTF8))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
             }
+
         }
 
     }
