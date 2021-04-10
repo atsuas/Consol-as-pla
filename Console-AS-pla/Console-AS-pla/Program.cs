daa8a9331a3ed0b21c5e06b6f7d4523735d6826f
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-11 06:00:14 +0900

    FileInfoのときはCopy to

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -10,8 +10,7 @@ namespace Exercise1
         static void Main(string[] args)
         {
             var fi = new FileInfo(@"C:\Example\Greeting.txt");
-            if (fi.Exists)
-                Console.WriteLine("既に存在しています");
+            FileInfo dup = fi.CopyTo(@"C:\Example\Target.txt", overwrite:true);
         }
 
     }
