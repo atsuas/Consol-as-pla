dc2ecc38dc00d2d0c3a80c3663393a77d35f37d6
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-11 05:42:54 +0900

    不要なコードの削除

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,19 +9,7 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            var filePath = @"C:\Example\Greeting.txt";
-            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
-            {
-                using (var reader = new StreamReader(stream))
-                using (var writer = new StreamWriter(stream))
-                {
-                    string texts = reader.ReadToEnd();
-                    stream.Position = 0;
-                    writer.WriteLine("挿入する新しい行1");
-                    writer.WriteLine("挿入する新しい行2");
-                    writer.Write(texts);
-                }
-            }
+            
         }
 
     }
