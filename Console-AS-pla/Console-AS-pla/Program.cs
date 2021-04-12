83cc9e4738239dfe16e5b88f880e67365624afcb
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-13 00:36:44 +0900

    指定したディレクトリ直下にあるファイルを別のディレクトリにコピーする

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -11,22 +11,34 @@ namespace Exercise1
         {
             if (args.Length <= 1)
                 return;
-            var file1 = args[0];
-            var file2 = args[1];
-            Concat(file1, file2);
 
-            Display(file1);
+            Console.WriteLine($"source: {Path.GetFullPath(args[0])}");
+            Console.WriteLine($"dest: {Path.GetFullPath(args[1])}\n");
+
+            var souceDir = args[0];
+            var destDir = args[1];
+            CopyFiles(souceDir, destDir);
+
         }
 
-        private static void Concat(string file1, string file2)
+        private static void CopyFiles(string sourceDir, string destDir)
         {
-            File.AppendAllLines(file1, File.ReadLines(file2));
+            var files = Directory.EnumerateFiles("*.*");
+            if (!Directory.Exists(destDir))
+                Directory.CreateDirectory(destDir);
+            foreach (var file in files)
+            {
+                var dest = GetBakFilePath(destDir, file);
+                Console.WriteLine(dest);
+                File.Copy(file, dest, overwrite: true);
+            }
         }
 
-        private static void Display(string outputPath)
+        private static string GetBakFilePath(string destDir, string file)
         {
-            var text = File.ReadAllText(outputPath);
-            Console.WriteLine(text);
+            var name = Path.GetFileNameWithoutExtension(file) + "_bak";
+            var ext = Path.GetExtension(file);
+            return Path.Combine(destDir, name + ext);
         }
     }
 
