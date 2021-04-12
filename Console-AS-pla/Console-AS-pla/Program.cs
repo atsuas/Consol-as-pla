c8622868a7ace0d8bad5936865b88b7393d1514d
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-12 23:12:13 +0900

    do while

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -9,44 +9,11 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-                return;
-            var file = args[0];
-
-            Console.WriteLine($"{Path.GetFullPath(file)}について調べます");
-            One(file);
-            Two(file);
-            Three(file);
-
-        }
-
-        private static void One(string file)
-        {
-            var count = 0;
-            using (var sr = new StreamReader(file))
+            bool finish;
+            do
             {
-                while(!sr.EndOfStream)
-                {
-                    var line = sr.ReadLine();
-                    if (line.Contains("class"))
-                        count++;
-                }
-            }
-            Console.WriteLine(count);
-        }
-
-        private static void Two(string file)
-        {
-            var count = File.ReadAllLines(file)
-                            .Count(c => c.Contains("class"));
-            Console.WriteLine(count);
-        }
-
-        private static void Three(string file)
-        {
-            var count = File.ReadLines(file)
-                            .Count(c => c.Contains("class"));
-            Console.WriteLine(count);
+                finish = Dosomething();
+            } while (!finish);
         }
 
     }
