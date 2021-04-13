90dbf55b9d0009b0715a37236714f522250a51e7
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-14 00:36:42 +0900

    DataContractSerializerを使ったシリアル化

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
 
 namespace Exercise1
 {
@@ -9,10 +11,36 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            
-
+            var novel = new Novel
+            {
+                Auther = "ジェイムズ",
+                Title = "星の砂",
+                Published = 1977,
+            };
+            var settings = new XmlWriterSettings
+            {
+                Encoding = new System.Text.UTF8Encoding(false),
+                Indent = true,
+                IndentChars = "  ",
+            };
+           using (var writer = XmlWriter.Create("novel.xml", settings))
+            {
+                var serializer = new DataContractSerializer(novel.GetType());
+                serializer.WriteObject(writer, novel);
+            }
         }
 
+    }
+
+    public class Novel
+    {
+        public string Title { get; set; }
+        public string Auther { get; set; }
+        public int Published { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"[Title={Title}, Auther:{Auther}, Published:{Published}]");
+        }
     }
 
 }
