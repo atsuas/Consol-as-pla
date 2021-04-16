cfe356c49d8e7b55d2bb6eb540b2cff83c588556
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-16 22:08:55 +0900

    DirectionaryからJSONデータへのシリアル化

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -15,11 +15,33 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            var target = "  non-whitespace characters   ";
-            var replaced = target.Trim();
-            Console.WriteLine($"{replaced}");
+            var abbreviationDict = new AbbreviationDict
+            {
+                Abbreviations = new Dictionary<string, string>
+                {
+                    ["ODA"] = "政府開発援助",
+                    ["OECD"] = "経済協力開発援助",
+                    ["OPEC"] = "石油輸出国機構",
+                }
+            };
+            var settings = new DataContractJsonSerializerSettings
+            {
+                UseSimpleDictionaryFormat = true,
+            };
+            using (var stream = new FileStream("abbreviations.json", FileMode.Create, FileAccess.Write))
+            {
+                var serializer = new DataContractJsonSerializer(abbreviationDict.GetType(), settings);
+                serializer.WriteObject(stream, abbreviationDict);
+            }
         }
 
+    }
+
+    [DataContract]
+    public class AbbreviationDict
+    {
+        [DataMember(Name = "abbrs")]
+        public Dictionary<string, string> Abbreviations { get; set; }
     }
 
 }
