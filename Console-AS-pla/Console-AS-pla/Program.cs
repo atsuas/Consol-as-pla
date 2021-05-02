5c2f6c1f9769d87291e5322cd020ff7e7bfbd0a7
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-05-03 03:09:14 +0900

    演習問題１

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -15,39 +15,18 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            if (args.Length <= 1)
-                return;
-
-            // これは確認用
-            Console.WriteLine($"source: {Path.GetFullPath(args[0])}");
-            Console.WriteLine($"dest:   {Path.GetFullPath(args[1])}\n");
-            // ここまで
-
-            var sourceDir = args[0];
-            var destDir = args[1];
-            CopyFiles(sourceDir, destDir);
-        }
-
-        private static void CopyFiles(string sourceDir, string destDir)
-        {
-            var files = Directory.EnumerateFiles(sourceDir, "*.*");
-            if (!Directory.Exists(destDir))
-                Directory.CreateDirectory(destDir);
-            foreach (var file in files)
+            var line = Console.ReadLine();
+            int num;
+            if (int.TryParse(line, out num))
             {
-                var dest = GetBakFilePath(destDir, file);
-                Console.WriteLine(dest);
-                File.Copy(file, dest, overwrite: true);
+                Console.WriteLine("{0:#,#}", num);
+            }
+            else
+            {
+                Console.WriteLine("数値文字列でありません");
             }
         }
 
-        private static string GetBakFilePath(string destDir, string file)
-        {
-            var name = Path.GetFileNameWithoutExtension(file) + "_bak";
-            var ext = Path.GetExtension(file);
-            // 拡張子がないファイルの場合、extは"" なので、無条件にextを追加してもうまくいく
-            return Path.Combine(destDir, name + ext);
-        }
     }
 
 }
