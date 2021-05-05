ef18b0cb9410b660fe003f39432f163f2fea221a
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-05-06 01:11:40 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using System.Runtime.Serialization;
-using System.Xml;
-using System.Xml.Serialization;
+using System.Linq;
 using System.Text;
-using System.Globalization;
-using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Exercise1
 {
@@ -15,29 +12,74 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-                return;
-            var file = args[0];
-            var outputPath = Numbering(file);
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
 
-            Display(outputPath);
-        }
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
 
-        private static string Numbering(string file)
-        {
-            var lines = File.ReadLines(file)
-                            .Select((s, n) => string.Format("{0,4}: {1}", n + 1, s));
-            var path = Path.ChangeExtension(file, ".txt");
-            File.WriteAllLines(path, lines);
-            return path;
-        }
-
-        // 確認用コード
-        private static void Display(string outputPath)
-        {
-            var text = File.ReadAllText(outputPath);
+            // これは確認用
+            var text = File.ReadAllText(newfile);
             Console.WriteLine(text);
         }
-    }
 
+        static void Exercise1_1(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements()
+                             .Select(x => new {
+                                 Name = x.Element("name").Value,
+                                 Teammembers = x.Element("teammembers").Value
+                             });
+            foreach (var sport in sports)
+            {
+                Console.WriteLine("{0} {1}", sport.Name, sport.Teammembers);
+            }
+        }
+        static void Exercise1_2(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements()
+                             .Select(x => new {
+                                 Firstplayed = x.Element("firstplayed").Value,
+                                 Name = x.Element("name").Attribute("kanji").Value
+                             })
+                             .OrderBy(x => int.Parse(x.Firstplayed));
+            foreach (var sport in sports)
+            {
+                Console.WriteLine("{0}", sport.Name);
+            }
+        }
+
+        static void Exercise1_3(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var sport = xdoc.Root.Elements()
+                             .Select(x => new {
+                                 Name = x.Element("name").Value,
+                                 Teammembers = x.Element("teammembers").Value
+                             })
+                             .OrderByDescending(x => int.Parse(x.Teammembers))
+                             .First();
+            Console.WriteLine("{0}", sport.Name);
+        }
+
+        static void Exercise1_4(string file, string newfile)
+        {
+            var xdoc = XDocument.Load(file);
+            var element = new XElement("ballsport",
+                 new XElement("name", "サッカー", new XAttribute("kanji", "蹴球")),
+                 new XElement("teammembers", "11"),
+                 new XElement("firstplayed", "1863")
+              );
+            xdoc.Root.Add(element);
+            xdoc.Save(newfile);
+        }
+
+    }
 }
