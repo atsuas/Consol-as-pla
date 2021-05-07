680404c1bc5e1ae55c8077a18b212d5270a5a8ce
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-05-07 20:46:20 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -4,82 +4,58 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
-namespace Exercise1
+namespace Exercise2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var file = "sample.xml";
-            Exercise1_1(file);
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            Exercise2_1(names);
             Console.WriteLine();
-            Exercise1_2(file);
+            Exercise2_2(names);
             Console.WriteLine();
-            Exercise1_3(file);
+            Exercise2_3(names);
             Console.WriteLine();
-
-            var newfile = "sports.xml";
-            Exercise1_4(file, newfile);
-
-            // これは確認用
-            var text = File.ReadAllText(newfile);
-            Console.WriteLine(text);
+            Exercise2_4(names);
         }
 
-        static void Exercise1_1(string file)
+        static void Exercise2_1(List<string> names)
         {
-            var xdoc = XDocument.Load(file);
-            var sports = xdoc.Root.Elements()
-                             .Select(x => new {
-                                 Name = x.Element("name").Value,
-                                 Teammembers = x.Element("teammembers").Value
-                             });
-            foreach (var sport in sports)
+            Console.WriteLine("都市名を入力。空行で終了");
+            do
             {
-                Console.WriteLine("{0} {1}", sport.Name, sport.Teammembers);
-            }
-        }
-        static void Exercise1_2(string file)
-        {
-            var xdoc = XDocument.Load(file);
-            var sports = xdoc.Root.Elements()
-                             .Select(x => new {
-                                 Firstplayed = x.Element("firstplayed").Value,
-                                 Name = x.Element("name").Attribute("kanji").Value
-                             })
-                             .OrderBy(x => int.Parse(x.Firstplayed));
-            foreach (var sport in sports)
-            {
-                Console.WriteLine("{0}", sport.Name);
-            }
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+                var index = names.FindIndex(s => s == line);
+                Console.WriteLine(index);
+            } while (true);
         }
 
-        static void Exercise1_3(string file)
+        static void Exercise2_2(List<string> names)
         {
-            var xdoc = XDocument.Load(file);
-            var sport = xdoc.Root.Elements()
-                             .Select(x => new {
-                                 Name = x.Element("name").Value,
-                                 Teammembers = x.Element("teammembers").Value
-                             })
-                             .OrderByDescending(x => int.Parse(x.Teammembers))
-                             .First();
-            Console.WriteLine("{0}", sport.Name);
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
         }
 
-        static void Exercise1_4(string file, string newfile)
+        static void Exercise2_3(List<string> names)
         {
-            var xdoc = XDocument.Load(file);
-            var element = new XElement("ballsport",
-                 new XElement("name", "サッカー", new XAttribute("kanji", "蹴球")),
-                 new XElement("teammembers", "11"),
-                 new XElement("firstplayed", "1863")
-              );
-            xdoc.Root.Add(element);
-            xdoc.Save(newfile);
+            var selected = names.Where(s => s.Contains('o'))
+                                .ToArray();
+            foreach (var name in selected)
+                Console.WriteLine(name);
         }
 
+        static void Exercise2_4(List<string> names)
+        {
+            var selected = names.Where(s => s.StartsWith("B"))
+                                .Select(s => s.Length);
+            foreach (var length in selected)
+                Console.WriteLine(length);
+        }
     }
 }
