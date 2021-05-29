822662ae8cd23536d3b2b1bf0b2008fd6c1eb0dc
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-05-29 20:49:44 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -4,36 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise2
+namespace Exercise1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+        }
+
+        private static void Exercise1_1(List<int> numbers)
+        {
+            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            if (exist)
+                Console.WriteLine("存在しています");
+            else
+                Console.WriteLine("存在していません");
+        }
+
+        private static void Exercise1_2(List<int> numbers)
+        {
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
+        }
+
+        private static void Exercise1_3(List<int> numbers)
+        {
+            foreach (var n in numbers.Where(x => x >= 50))
             {
-                var dt = new DateTime(2017, 1, 1);
-                foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek)))
-                {
-                    Console.Write("{0:yy/MM/dd}の次週の{1}: ", dt, (DayOfWeek)dayofweek);
-                    Console.WriteLine("{0:yy/MM/dd(ddd)}", NextWeek(dt, (DayOfWeek)dayofweek));
-                }
-            }
-            Console.WriteLine();
-            {
-                var dt = new DateTime(2017, 4, 30);
-                foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek)))
-                {
-                    Console.Write("{0:yy/MM/dd}の次週の{1}: ", dt, (DayOfWeek)dayofweek);
-                    Console.WriteLine("{0:yy/MM/dd(ddd)}", NextWeek(dt, (DayOfWeek)dayofweek));
-                }
+                Console.WriteLine(n);
             }
         }
 
-        public static DateTime NextWeek(DateTime date, DayOfWeek dayOfWeek)
+        private static void Exercise1_4(List<int> numbers)
         {
-            var nextweek = date.AddDays(7);
-            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
-            return nextweek.AddDays(days);
+            var list = numbers.Select(n => n * 2).ToList();
+            foreach (var n in list)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
