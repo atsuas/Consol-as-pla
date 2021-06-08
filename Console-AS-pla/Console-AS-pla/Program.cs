7128a80c1487223e75efeff87a50f83d1d4408c1
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-06-08 20:43:47 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,35 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace Exercise3
+namespace Exercise2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var tw = new TimeWatch();
-            tw.Start();
-            Thread.Sleep(1000);
-            TimeSpan duration = tw.Stop();
-            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
-        }
-    }
-
-    class TimeWatch
-    {
-        private DateTime _time;
-        public void Start()
-        {
-            _time = DateTime.Now;
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            Exercise2_1(names);
+            Console.WriteLine();
+            Exercise2_2(names);
+            Console.WriteLine();
+            Exercise2_3(names);
+            Console.WriteLine();
+            Exercise2_4(names);
         }
 
-        public TimeSpan Stop()
+        static void Exercise2_1(List<string> names)
         {
-            return DateTime.Now - _time;
+            Console.WriteLine("都市名を入力。空行で終了");
+            do
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+                var index = names.FindIndex(s => s == line);
+                Console.WriteLine(index);
+            } while (true);
+        }
+
+        static void Exercise2_2(List<string> names)
+        {
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
+        }
+
+        static void Exercise2_3(List<string> names)
+        {
+            var selected = names.Where(s => s.Contains('o'))
+                                .ToArray();
+            foreach (var name in selected)
+                Console.WriteLine(name);
+        }
+
+        static void Exercise2_4(List<string> names)
+        {
+            var selected = names.Where(s => s.StartsWith("B"))
+                                .Select(s => s.Length);
+            foreach (var length in selected)
+                Console.WriteLine(length);
         }
     }
 }
