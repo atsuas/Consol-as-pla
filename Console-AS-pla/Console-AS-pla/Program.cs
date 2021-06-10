2d7c45405271c02d102a7f785f86e52ac052a9e5
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-06-10 19:22:31 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,61 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise2
+namespace Exercise1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var names = new List<string> {
-                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
-            };
-            Exercise2_1(names);
-            Console.WriteLine();
-            Exercise2_2(names);
-            Console.WriteLine();
-            Exercise2_3(names);
-            Console.WriteLine();
-            Exercise2_4(names);
-        }
-
-        static void Exercise2_1(List<string> names)
-        {
-            Console.WriteLine("都市名を入力。空行で終了");
-            do
-            {
-                var line = Console.ReadLine();
-                if (string.IsNullOrEmpty(line))
-                    break;
-                var index = names.FindIndex(s => s == line);
-                Console.WriteLine(index);
-            } while (true);
-        }
-
-        static void Exercise2_2(List<string> names)
-        {
-            var count = names.Count(s => s.Contains('o'));
-            Console.WriteLine(count);
-        }
-
-        static void Exercise2_3(List<string> names)
-        {
-            var selected = names.Where(s => s.Contains('o'))
-                                .ToArray();
-            foreach (var name in selected)
-                Console.WriteLine(name);
-        }
-
-        static void Exercise2_4(List<string> names)
-        {
-            var selected = names.Where(s => s.StartsWith("B"))
-                                .Select(s => s.Length);
-            foreach (var length in selected)
-                Console.WriteLine(length);
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+            if (string.Compare(s1, s2, ignoreCase: true) == 0)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
         }
     }
 }
