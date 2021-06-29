9a9ac9f8c93770dd8d8f0db0a059b5a020601c60
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-06-29 23:27:45 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise1
@@ -10,54 +11,21 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
-
-            // 3.1.1
-            Exercise1_1(numbers);
-            Console.WriteLine("-----");
-
-            // 3.1.2
-            Exercise1_2(numbers);
-            Console.WriteLine("-----");
-
-            // 3.1.3
-            Exercise1_3(numbers);
-            Console.WriteLine("-----");
-
-            // 3.1.4
-            Exercise1_4(numbers);
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("080-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("060-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("190-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("091-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-12341"));
+            Console.WriteLine(IsPhoneNumber("A090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-911-1234"));
+            Console.WriteLine(IsPhoneNumber("090-1911-234"));
         }
 
-        private static void Exercise1_1(List<int> numbers)
+        static bool IsPhoneNumber(string str)
         {
-            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
-            if (exist)
-                Console.WriteLine("存在しています");
-            else
-                Console.WriteLine("存在していません");
-        }
-
-        private static void Exercise1_2(List<int> numbers)
-        {
-            numbers.ForEach(n => Console.WriteLine(n / 2.0));
-        }
-
-        private static void Exercise1_3(List<int> numbers)
-        {
-            foreach (var n in numbers.Where(x => x >= 50))
-            {
-                Console.WriteLine(n);
-            }
-        }
-
-        private static void Exercise1_4(List<int> numbers)
-        {
-            var list = numbers.Select(n => n * 2).ToList();
-            foreach (var n in list)
-            {
-                Console.WriteLine(n);
-            }
+            return Regex.IsMatch(str, @"^0[789]0-\d{4}-\d{4}$");
         }
     }
 }
-
