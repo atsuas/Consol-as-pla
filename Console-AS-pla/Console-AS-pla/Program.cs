058c65789b9f97fbaa08b8d795cba580e2166d07
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-07-02 21:07:10 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -3,81 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Chapter04;
 
-namespace Exercise2
+namespace Exercise1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // 4.2.1
-            var ymCollection = new YearMonth[] {
-                new YearMonth(1980, 1),
-                new YearMonth(1990, 4),
-                new YearMonth(2000, 7),
-                new YearMonth(2010, 9),
-                new YearMonth(2020, 12),
+            // 2.1.3
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
             };
+            PrintSongs(songs);
 
-            // 4.2.2
-            Exercise2_2(ymCollection);
-            Console.WriteLine("----");
-
-            // 4.2.4
-            Exercise2_4(ymCollection);
-            Console.WriteLine("----");
-
-
-            // 4.2.5
-            Exercise2_5(ymCollection);
         }
 
-        // 4.2.3
-        static YearMonth FindFirst21C(YearMonth[] yms)
+        // 2.1.4
+        private static void PrintSongs(Song[] songs)
         {
-            foreach (var ym in yms)
+            foreach (var song in songs)
             {
-                if (ym.Is21Century)
-                    return ym;
-            }
-            return null;
-        }
-
-        private static void Exercise2_2(YearMonth[] ymCollection)
-        {
-            foreach (var ym in ymCollection)
-            {
-                Console.WriteLine(ym);
+                Console.WriteLine(@"{0}, {1} {2:m\:ss}",
+                    song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
             }
         }
 
-        private static void Exercise2_4(YearMonth[] ymCollection)
-        {
-            var yearmonth = FindFirst21C(ymCollection);
-            if (yearmonth == null)
-                Console.WriteLine("21世紀のデータはありません");
-            else
-                Console.WriteLine(yearmonth);
-
-
-            // あるいは、以下のような書き方もできる
-            Console.WriteLine("----");
-            var yearmonth2 = FindFirst21C(ymCollection);
-            var s = yearmonth2 == null ? "21世紀のデータはありません" : yearmonth2.ToString();
-            Console.WriteLine(s);
-        }
-
-
-        private static void Exercise2_5(YearMonth[] ymCollection)
-        {
-            var array = ymCollection.Select(ym => ym.AddOneMonth())
-                                    .ToArray();
-            foreach (var ym in array)
-            {
-                Console.WriteLine(ym);
-            }
-        }
+        /*
+           @"{0}, {1} {2:m\:ss}" について
+           {} の中で、:は特別な意味を持っている。そのため、: を 文字':'として表示させるために
+           \: としている。なお、\: をエスケープシーケンスと認識させないように、@を先頭に付加し、
+           逐語的リテラル文字列にしている。     
+        */
     }
 
 }
