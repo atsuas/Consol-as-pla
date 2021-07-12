e83fab5ab564241e4c2b79b5a65383965478c5a8
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-07-12 21:13:35 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,43 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
-namespace Exercise1
+namespace Exercise2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // 2.1.3
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
-            PrintSongs(songs);
+            var novelist = Exercise2_1("sample.xml");
+            Exercise2_2(novelist, "novelist.json");
 
+            // これは確認のためのコード 12.2.1
+            Console.WriteLine("{0} {1}", novelist.Name, novelist.Birth);
+            foreach (var title in novelist.Masterpieces)
+            {
+                Console.WriteLine(title);
+            }
+            Console.WriteLine();
+
+            // これは確認のためのコード 12.2.2
+            Console.WriteLine(File.ReadAllText("novelist.json"));
+            Console.WriteLine();
         }
 
-        // 2.1.4
-        private static void PrintSongs(Song[] songs)
+        static Novelist Exercise2_1(string file)
         {
-            foreach (var song in songs)
+            using (var reader = XmlReader.Create(file))
             {
-                Console.WriteLine(@"{0}, {1} {2:m\:ss}",
-                    song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+                var serializer = new XmlSerializer(typeof(Novelist));
+                var novelist = (Novelist)serializer.Deserialize(reader);
+
+                return novelist;
             }
         }
 
-        /*
-           @"{0}, {1} {2:m\:ss}" について
-           {} の中で、:は特別な意味を持っている。そのため、: を 文字':'として表示させるために
-           \: としている。なお、\: をエスケープシーケンスと認識させないように、@を先頭に付加し、
-           逐語的リテラル文字列にしている。     
-        */
+        static void Exercise2_2(Novelist novelist, string outfile)
+        {
+            using (var stream = new FileStream(outfile, FileMode.Create,
+                                                FileAccess.Write))
+            {
+                var serializer = new DataContractJsonSerializer(novelist.GetType(),
+                                                        new DataContractJsonSerializerSettings
+                                                        {
+                                                            DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                                                        });
+                serializer.WriteObject(stream, novelist);
+            }
+        }
+    }
+
+    [XmlRoot("novelist")]
+    [DataContract]
+    public class Novelist
+    {
+        [XmlElement(ElementName = "name")]
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "birth")]
+        [DataMember(Name = "birth")]
+        public DateTime Birth { get; set; }
+
+        [XmlArray("masterpieces")]
+        [XmlArrayItem("title", typeof(string))]
+        [DataMember(Name = "masterpieces")]
+        public string[] Masterpieces { get; set; }
     }
 
 }
