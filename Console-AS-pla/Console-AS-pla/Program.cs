b045e0ba2dd436d327d324b4fbc859ccbc4593ac
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-07-16 20:47:43 +0900

    plactice

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,30 +11,51 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            {
-                var dt = new DateTime(2017, 1, 1);
-                foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek)))
-                {
-                    Console.Write("{0:yy/MM/dd}の次週の{1}: ", dt, (DayOfWeek)dayofweek);
-                    Console.WriteLine("{0:yy/MM/dd(ddd)}", NextWeek(dt, (DayOfWeek)dayofweek));
-                }
-            }
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            Exercise2_1(names);
             Console.WriteLine();
-            {
-                var dt = new DateTime(2017, 4, 30);
-                foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek)))
-                {
-                    Console.Write("{0:yy/MM/dd}の次週の{1}: ", dt, (DayOfWeek)dayofweek);
-                    Console.WriteLine("{0:yy/MM/dd(ddd)}", NextWeek(dt, (DayOfWeek)dayofweek));
-                }
-            }
+            Exercise2_2(names);
+            Console.WriteLine();
+            Exercise2_3(names);
+            Console.WriteLine();
+            Exercise2_4(names);
         }
 
-        public static DateTime NextWeek(DateTime date, DayOfWeek dayOfWeek)
+        static void Exercise2_1(List<string> names)
         {
-            var nextweek = date.AddDays(7);
-            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
-            return nextweek.AddDays(days);
+            Console.WriteLine("都市名を入力。空行で終了");
+            do
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+                var index = names.FindIndex(s => s == line);
+                Console.WriteLine(index);
+            } while (true);
+        }
+
+        static void Exercise2_2(List<string> names)
+        {
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
+        }
+
+        static void Exercise2_3(List<string> names)
+        {
+            var selected = names.Where(s => s.Contains('o'))
+                                .ToArray();
+            foreach (var name in selected)
+                Console.WriteLine(name);
+        }
+
+        static void Exercise2_4(List<string> names)
+        {
+            var selected = names.Where(s => s.StartsWith("B"))
+                                .Select(s => s.Length);
+            foreach (var length in selected)
+                Console.WriteLine(length);
         }
     }
 }
