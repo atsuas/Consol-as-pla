2b3c3556a33ea910fe1ab898d0d232ef1a9483cc
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-08-06 07:26:14 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -2,34 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Exercise4
+namespace Exercise3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            foreach (var pair in line.Split(';'))
-            {
-                var array = pair.Split('=');
-                Console.WriteLine("{0}:{1}", ToJapanese(array[0]), array[1]);
-            }
-        }
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
 
-        static string ToJapanese(string key)
-        {
-            switch (key)
+            foreach (var line in texts)
             {
-                case "Novelist":
-                    return "作家　";
-                case "BestWork":
-                    return "代表作";
-                case "Born":
-                    return "誕生年";
+                var matches = Regex.Matches(line, @"\btime\b", RegexOptions.IgnoreCase);
+                foreach (Match m in matches)
+                {
+                    Console.WriteLine("{0}: {1}", line, m.Index);
+                }
             }
-            throw new ArgumentException("引数keyは、正しい値ではありません");
         }
     }
 }
