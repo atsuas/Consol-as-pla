fed58a6592eee581aac4ecf572aa842f762c54e8
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-08-31 08:07:50 +0900

    演習問題

--- Console-AS-pla/Console-AS-pla/Program.cs
+++ Console-AS-pla/Console-AS-pla/Program.cs
@@ -1,48 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise4
+namespace Exercise2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length <= 1)
-                return;
+            var books = new List<Book> {
+               new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+               new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+               new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+               new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+               new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+               new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
+               new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+            };
 
-            // これは確認用
-            Console.WriteLine($"source: {Path.GetFullPath(args[0])}");
-            Console.WriteLine($"dest:   {Path.GetFullPath(args[1])}\n");
-            // ここまで
+            Exercise2_1(books);
+            Console.WriteLine("-----");
 
-            var sourceDir = args[0];
-            var destDir = args[1];
-            CopyFiles(sourceDir, destDir);
+            Exercise2_2(books);
+
+            Console.WriteLine("-----");
+
+            Exercise2_3(books);
+            Console.WriteLine("-----");
+
+            Exercise2_4(books);
+            Console.WriteLine("-----");
+
+            Exercise2_5(books);
+            Console.WriteLine("-----");
+
+            Exercise2_6(books);
+
+            Console.WriteLine("-----");
+
+            Exercise2_7(books);
         }
 
-        private static void CopyFiles(string sourceDir, string destDir)
+        private static void Exercise2_1(List<Book> books)
         {
-            var files = Directory.EnumerateFiles(sourceDir, "*.*");
-            if (!Directory.Exists(destDir))
-                Directory.CreateDirectory(destDir);
-            foreach (var file in files)
+            var book = books.FirstOrDefault(b => b.Title == "ワンダフル・C#ライフ");
+            if (book != null)
+                Console.WriteLine("{0} {1}", book.Price, book.Pages);
+        }
+
+        private static void Exercise2_2(List<Book> books)
+        {
+            int count = books.Count(b => b.Title.Contains("C#"));
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2_3(List<Book> books)
+        {
+            var average = books.Where(b => b.Title.Contains("C#"))
+                               .Average(b => b.Pages);
+            Console.WriteLine(average);
+        }
+
+        private static void Exercise2_4(List<Book> books)
+        {
+            var book = books.FirstOrDefault(b => b.Price >= 4000);
+            if (book != null)
+                Console.WriteLine(book.Title);
+        }
+
+        private static void Exercise2_5(List<Book> books)
+        {
+            var pages = books.Where(b => b.Price < 4000)
+                             .Max(b => b.Pages);
+            Console.WriteLine(pages);
+        }
+
+        private static void Exercise2_6(List<Book> books)
+        {
+            var selected = books.Where(b => b.Pages >= 400)
+                                .OrderByDescending(b => b.Price);
+            foreach (var book in selected)
             {
-                var dest = GetBakFilePath(destDir, file);
-                Console.WriteLine(dest);
-                File.Copy(file, dest, overwrite: true);
+                Console.WriteLine("{0} {1}", book.Title, book.Price);
             }
         }
 
-        private static string GetBakFilePath(string destDir, string file)
+        private static void Exercise2_7(List<Book> books)
         {
-            var name = Path.GetFileNameWithoutExtension(file) + "_bak";
-            var ext = Path.GetExtension(file);
-            // 拡張子がないファイルの場合、extは"" なので、無条件にextを追加してもうまくいく
-            return Path.Combine(destDir, name + ext);
+            var selected = books.Where(b => b.Title.Contains("C#") && b.Pages <= 500);
+            foreach (var book in selected)
+                Console.WriteLine(book.Title);
         }
     }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
+    }
+
 }
